fa9e2608d217143a7ab9b8bc452291972b7246ad
Pankajghz <135798617+Pankajghz@users.noreply.github.com>
2024-07-01 11:23:02 +0530

    Update Preference.jsx

--- src/components/Preference.jsx
+++ src/components/Preference.jsx
@@ -1,73 +1,121 @@
-import React, { useState, useEffect } from 'react';
-import '../styles/Profile.css';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "../styles/Profile.css";
+import { useNavigate } from "react-router-dom";
 
 const Preference = () => {
-    const [submitted, setSubmitted] = useState(false);
-    const navigate = useNavigate();
+  const [submitted, setSubmitted] = useState(false);
+  const navigate = useNavigate();
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log("Thik hai");
-        setSubmitted(true);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Thik hai");
+    setSubmitted(true);
+  };
+
+  useEffect(() => {
+    if (submitted === true) {
+      navigate("/");
     }
+  }, [submitted, navigate]);
 
-
-    useEffect(() => {
-        if (submitted === true) {
-            navigate("/");
-        }
-    }, [submitted, navigate]);
-
-    return (
-        <>
-            <div id="profile-body">
-                <div className="container">
-                    <div className="title">List your crush</div>
-                    <div className="content">
-                        <form onSubmit={handleSubmit}>
-                            <div className="user-details">
-                                <div className="input-box">
-                                    <span className="details">Insta id</span>
-                                    <input type="text" placeholder="Enter Insta Id" maxLength="20" required />
-                                    <span className="details">Name</span>
-                                    <input type="text" placeholder="Enter name" maxLength="25" required />
-                                </div>
-
-
-                                <div className="input-box">
-                                    <span className="details">Insta id</span>
-                                    <input type="text" placeholder="Enter Insta Id" maxLength="20" required />
-                                    <span className="details">Name</span>
-                                    <input type="text" placeholder="Enter name" maxLength="25" required />
-                                </div>
-
-                                <div className="input-box">
-                                    <span className="details">Insta id</span>
-                                    <input type="text" placeholder="Enter Insta Id" maxLength="20" required />
-                                    <span className="details">Name</span>
-                                    <input type="text" placeholder="Enter name" maxLength="25" required />
-                                </div>
-
-
-                                <div className="input-box">
-                                    <span className="details">Insta id</span>
-                                    <input type="text" placeholder="Enter Insta Id" maxLength="20" required />
-                                    <span className="details">Name</span>
-                                    <input type="text" placeholder="Enter name" maxLength="25" required />
-                                </div>
-
-                            </div>
-
-                            <div className="button"  >
-                                <input type="submit" value="Save" />
-                            </div>
-                        </form>
-                    </div>
+  return (
+    <>
+      <div id="profile-body">
+        <div className="container">
+          <div className="title">List your crush</div>
+          <div className="content">
+            <form onSubmit={handleSubmit}>
+              <div className="user-details">
+                <div className="input-box">
+                  <span className="details">Full Name</span>
+                  <input
+                    type="text"
+                    name="name1"
+                    placeholder="Enter name"
+                    maxLength="25"
+                    required
+                  />
+                  <span className="details">Phone Number</span>
+                  <input
+                    type="number"
+                    name="phone1"
+                    placeholder="Enter your number"
+                    min="1000000000"
+                    max="9999999999"
+                    required
+                  />
                 </div>
-            </div>
-        </>
-    );
+
+                <div className="input-box">
+                  <span className="details">Full Name</span>
+                  <input
+                    type="text"
+                    name="name2"
+                    placeholder="Enter name"
+                    maxLength="25"
+                    required
+                  />
+                  <span className="details">Phone Number</span>
+                  <input
+                    type="number"
+                    name="phone2"
+                    placeholder="Enter your number"
+                    min="1000000000"
+                    max="9999999999"
+                    required
+                  />
+                </div>
+
+                <div className="input-box">
+                  <span className="details">Full Name</span>
+                  <input
+                    type="text"
+                    name="name3"
+                    placeholder="Enter name"
+                    maxLength="25"
+                    required
+                  />
+                  <span className="details">Phone Number</span>
+                  <input
+                    type="number"
+                    name="phone3"
+                    placeholder="Enter your number"
+                    min="1000000000"
+                    max="9999999999"
+                    required
+                  />
+                </div>
+
+                <div className="input-box">
+                  <span className="details">Full Name</span>
+                  <input
+                    type="text"
+                    name="name4"
+                    placeholder="Enter name"
+                    maxLength="25"
+                    required
+                  />
+                  <span className="details">Phone Number</span>
+                  <input
+                    type="number"
+                    name="phone4"
+                    placeholder="Enter your number"
+                    min="1000000000"
+                    max="9999999999"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div className="button">
+                <input type="submit" value="Save" />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Preference;
