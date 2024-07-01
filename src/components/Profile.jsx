93ddbc6f9dae3c39e0a82412bb64456beb805cb4
Pankajghz <135798617+Pankajghz@users.noreply.github.com>
2024-07-01 11:27:39 +0530

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,19 +2,52 @@ import React, { useState, useEffect } from "react";
 import "../styles/Profile.css";
 import { useNavigate } from "react-router-dom";
 
-const Preference = () => {
+const Profile = () => {
+  document.title = "Matchup create account";
+
+  const [form, setForm] = useState({});
   const [submitted, setSubmitted] = useState(false);
+  const [passwordError, setPasswordError] = useState("");
   const navigate = useNavigate();
+
+  const validatePassword = () => {
+    if (form.password !== form.passwordConf) {
+      setPasswordError("Passwords don't match");
+      return false;
+    }
+    setPasswordError("");
+    return true;
+  };
+
+  const togglePasswordVisibility = () => {
+    const password = document.getElementById("password");
+    const passwordConf = document.getElementById("confirm_password");
+    password.type = password.type === "password" ? "text" : "password";
+    passwordConf.type = passwordConf.type === "password" ? "text" : "password";
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("Thik hai");
-    setSubmitted(true);
+    const isPasswordValid = validatePassword();
+
+    if (isPasswordValid) {
+      console.log(form);
+      setForm({});
+      setSubmitted(true);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setForm((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
   };
 
   useEffect(() => {
     if (submitted === true) {
-      navigate("/");
+      navigate("/otp");
     }
   }, [submitted, navigate]);
 
@@ -22,7 +55,7 @@ const Preference = () => {
     <>
       <div id="profile-body">
         <div className="container">
-          <div className="title">List your crush</div>
+          <div className="title">Signup</div>
           <div className="content">
             <form onSubmit={handleSubmit}>
               <div className="user-details">
@@ -30,85 +63,102 @@ const Preference = () => {
                   <span className="details">Full Name</span>
                   <input
                     type="text"
-                    name="name1"
-                    placeholder="Enter name"
+                    name="name"
+                    placeholder="Enter your name"
                     maxLength="25"
-                    required
-                  />
-                  <span className="details">Phone Number</span>
-                  <input
-                    type="number"
-                    name="phone1"
-                    placeholder="Enter your number"
-                    min="1000000000"
-                    max="9999999999"
+                    onChange={handleInputChange}
+                    value={form.name || ""}
                     required
                   />
                 </div>
 
                 <div className="input-box">
-                  <span className="details">Full Name</span>
-                  <input
-                    type="text"
-                    name="name2"
-                    placeholder="Enter name"
-                    maxLength="25"
-                    required
-                  />
                   <span className="details">Phone Number</span>
                   <input
                     type="number"
-                    name="phone2"
+                    name="phone"
                     placeholder="Enter your number"
                     min="1000000000"
                     max="9999999999"
+                    onChange={handleInputChange}
+                    value={form.phone || ""}
                     required
                   />
                 </div>
-
                 <div className="input-box">
-                  <span className="details">Full Name</span>
+                  <span className="details">Password</span>
                   <input
-                    type="text"
-                    name="name3"
-                    placeholder="Enter name"
-                    maxLength="25"
-                    required
-                  />
-                  <span className="details">Phone Number</span>
-                  <input
-                    type="number"
-                    name="phone3"
-                    placeholder="Enter your number"
-                    min="1000000000"
-                    max="9999999999"
+                    type="password"
+                    name="password"
+                    placeholder="Enter your password"
+                    id="password"
+                    minLength="6"
+                    maxLength="15"
+                    onChange={handleInputChange}
+                    value={form.password || ""}
                     required
                   />
                 </div>
-
                 <div className="input-box">
-                  <span className="details">Full Name</span>
+                  <span className="details">Confirm Password</span>
                   <input
-                    type="text"
-                    name="name4"
-                    placeholder="Enter name"
-                    maxLength="25"
+                    type="password"
+                    name="passwordConf"
+                    placeholder="Confirm your password"
+                    id="confirm_password"
+                    minLength="6"
+                    maxLength="15"
+                    onChange={handleInputChange}
+                    value={form.passwordConf || ""}
                     required
                   />
-                  <span className="details">Phone Number</span>
-                  <input
-                    type="number"
-                    name="phone4"
-                    placeholder="Enter your number"
-                    min="1000000000"
-                    max="9999999999"
-                    required
-                  />
+                  {passwordError && (
+                    <div className="error">{passwordError}</div>
+                  )}
+                </div>
+              </div>
+              <input type="checkbox" onClick={togglePasswordVisibility} />
+              <div className="gender-details">
+                <input
+                  type="radio"
+                  name="gender"
+                  onChange={handleInputChange}
+                  value={"Male"}
+                  id="dot-1"
+                />
+                <input
+                  type="radio"
+                  name="gender"
+                  onChange={handleInputChange}
+                  value={"Female"}
+                  id="dot-2"
+                />
+                <input
+                  type="radio"
+                  name="gender"
+                  onChange={handleInputChange}
+                  value={"Prefer not to say"}
+                  id="dot-3"
+                />
+                <span className="gender-title">Gender</span>
+                <div className="category">
+                  <label htmlFor="dot-1">
+                    <span className="dot one"></span>
+                    <span className="gender">Male</span>
+                  </label>
+                  <label htmlFor="dot-2">
+                    <span className="dot two"></span>
+                    <span className="gender">Female</span>
+                  </label>
+                  <label htmlFor="dot-3">
+                    <span className="dot three"></span>
+                    <span className="gender">Prefer not to say</span>
+                  </label>
                 </div>
               </div>
 
               <div className="button">
-                <input type="submit" value="Save" />
+                <input type="submit" value="Signup" />
               </div>
             </form>
           </div>
@@ -118,4 +168,4 @@ const Preference = () => {
   );
 };
 
-export default Preference;
+export default Profile;
