adb4f427f16d2b6c2f1c5bced75c3d39abfc98b6
Pankajghz <135798617+Pankajghz@users.noreply.github.com>
2024-07-01 12:02:19 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,38 @@
-import React from 'react';
-import '../styles/Home.css'
+import React from "react";
+import "../styles/Home.css";
 
-import { Link } from 'react-router-dom';
-import Navbar from './Navbar'
-import { useEffect } from 'react';
+import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
+import { useEffect } from "react";
 
 const Home = () => {
+  useEffect(() => {
+    document.title = "Matchup";
+  }, []);
 
-    useEffect(() => {
-        document.title = 'Matchup';
-    }, [])
-
-    return (
-        <>
-            <Navbar />
-            <div className="img">
-                <div className="maincontent">
-                    <p>Get your</p>
-                    <p>Perfect Match</p>
-                </div>
-                <Link to="/profile">
-                    <button type="button" className="btn btn-danger bg-black">Create Account</button>
-                </Link>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <div className="img">
+        <div className="maincontent">
+          <p>Get your</p>
+          <p>Perfect Match</p>
+        </div>
+        <Link to="/profile">
+          <button type="button" className="btn btn-danger bg-black">
+            Create Account
+          </button>
+        </Link>
+        <div>
+          <Link to="/Login">
+            <button type="button" className="btn btn-danger bg-black">
+              Login
+            </button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
